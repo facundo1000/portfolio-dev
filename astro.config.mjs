e6d4044c43cddf5089ea5f02c509f73b1fcf411e
facundo1000 <martinez.facundo85@gmail.com>
2024-09-14 15:28:33 -0300

    proyect init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://portfolio-dev-bice-chi.vercel.app/',
+  integrations: [mdx(), sitemap(), tailwind()],
+
 });
